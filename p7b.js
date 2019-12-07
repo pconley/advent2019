06295c15556c25f3965e9af7dccd09d586fa0191
Pat Conley <pconley312@gmail.com>
2019-12-07 16:22:41 -0500

    day7 complete

--- p7b.js
+++ p7b.js
@@ -41,45 +41,45 @@ function val(mem,pos,mode){
   if( mode == ADR ){
     // mode 0: address mode; aka interpreted as a address
     result = mem[mem[pos]]
-    console.debug(`val: pos=${pos} mode=${mode} addr=${mem[pos]} >> result=${result}`)
+    // console.debug(`val: pos=${pos} mode=${mode} addr=${mem[pos]} >> result=${result}`)
   } else {
     // mode 1: immediate mode; aka interpreted as a value
     result = mem[pos]
-    console.debug(`val: pos=${pos} mode=${mode} >> result=${result}`)
-
+    // console.debug(`val: pos=${pos} mode=${mode} >> result=${result}`)
   }
   return result;
 }
 
-var index = 0;
-var input = [];
+
 var output = 999;
 
-function run(memory){
+function run(state, input){
+
+  var index = 0;
 
   const commands = {
     1 : (m,p) => {
       // add has 3 parameters
       const modes = extract(memory[pos]);
-      console.log("add.  memory=",m[p+0],m[p+1],m[p+2],m[p+3]);
-      console.log("add.  modes =",(modes[0]),modes[1],modes[2]);
+      // console.log("add.  memory=",m[p+0],m[p+1],m[p+2],m[p+3]);
+      // console.log("add.  modes =",(modes[0]),modes[1],modes[2]);
       const v1 = val(m,pos+1,modes[0]);
       const v2 = val(m,pos+2,modes[1]);
-      console.log("add:",v1,"+",v2,"into pos",m[p+3]);
+      // console.log("add:",v1,"+",v2,"into pos",m[p+3]);
       m[m[p+3]] = v1 + v2;
-      console.log(`result at m[${m[p+3]}] = ${m[m[p+3]]}`)
+      // console.log(`result at m[${m[p+3]}] = ${m[m[p+3]]}`)
       return p+4;
     },
     2 : (m,p) => {
       // mul has 3 parameters
       const modes = extract(memory[pos]);
-      console.log("mul.  memory=",m[p+0],m[p+1],m[p+2],m[p+3]);
-      console.log("mul.  modes =",modes[0],modes[1],modes[2]);
+      // console.log("mul.  memory=",m[p+0],m[p+1],m[p+2],m[p+3]);
+      // console.log("mul.  modes =",modes[0],modes[1],modes[2]);
       const v1 = val(m,pos+1,modes[0])
       const v2 = val(m,pos+2,modes[1])
-      console.log("mul:",v1,"*",v2,"into pos",m[p+3]);
+      // console.log("mul:",v1,"*",v2,"into pos",m[p+3]);
       m[m[p+3]] = v1 * v2;
-      console.log(`result at m[${m[p+3]}] = ${m[m[p+3]]}`)
+      // console.log(`result at m[${m[p+3]}] = ${m[m[p+3]]}`)
       return p+4;
     },
     // Opcode 3 takes a single integer as input and saves it to the position 
@@ -94,24 +94,25 @@ function run(memory){
     // instruction 4,50 would out the value at address 50.
     4 : (m,p) => {
       const modes = extract(memory[pos]);
-      console.log("out  memory=",m[p+0],m[p+1]);
-      console.log("out  modes =",modes);
+      // console.log("out  memory=",m[p+0],m[p+1]);
+      // console.log("out  modes =",modes);
       output = val(m,p+1,modes[0]); // a global
+      console.log(`OUTPUT: ${output}`);
       return p+2;
     },
     // Opcode 5 is jump-if-true: if the first parameter is non-zero, it sets the instruction 
     // pointer to the value from the second parameter. Otherwise, it does nothing.
     5 : (m,p) => {
       const modes = extract(memory[pos]);
-      console.log("jump-if-true memory=",m[p+0],m[p+1],m[p+1]);
-      console.log("jump-if-true modes =",modes);
+      // console.log("jump-if-true memory=",m[p+0],m[p+1],m[p+1]);
+      // console.log("jump-if-true modes =",modes);
       const v1 = val(m,pos+1,modes[0]);
       if( v1 == 0 ){
-        console.log("no jump");
+        // console.log("no jump");
         return p+3; // next command
       } else {
         const v2 = val(m,pos+2,modes[1]);
-        console.log(`jump to ${v2}`);
+        // console.log(`jump to ${v2}`);
         return v2;
       }
     },
@@ -119,15 +120,15 @@ function run(memory){
     // pointer to the value from the second parameter. Otherwise, it does nothing.
     6 : (m,p) => {
       const modes = extract(memory[pos]);
-      console.log("jump-if-false memory=",m[p+0],m[p+1],m[p+1]);
-      console.log("jump-if-false modes =",modes);
+      // console.log("jump-if-false memory=",m[p+0],m[p+1],m[p+1]);
+      // console.log("jump-if-false modes =",modes);
       const v1 = val(m,pos+1,modes[0]);
       if( v1 != 0 ){
-        console.log("no jump");
+        // console.log("no jump");
         return p+3; // next command
       } else {
         const v2 = val(m,pos+2,modes[1]);
-        console.log(`jump to ${v2}`);
+        // console.log(`jump to ${v2}`);
         return v2;
       }
     },
@@ -135,12 +136,12 @@ function run(memory){
     // it stores 1 in the position given by the third parameter. Otherwise, it stores 0.
     7 : (m,p) => {
       const modes = extract(memory[pos]);
-      console.log("less-than memory=",m[p+0],m[p+1],m[p+1]);
-      console.log("less-than modes =",modes);
+      // console.log("less-than memory=",m[p+0],m[p+1],m[p+1]);
+      // console.log("less-than modes =",modes);
       const v1 = val(m,pos+1,modes[0]);
       const v2 = val(m,pos+2,modes[1]);
       result = (v1 < v2) ? 1 : 0;
-      console.log(`setting ${m[m[p+3]]} to ${result}`)
+      // console.log(`setting ${m[m[p+3]]} to ${result}`)
       m[m[p+3]] = result;
       return p+4; // next command
     },
@@ -148,77 +149,35 @@ function run(memory){
     // it stores 1 in the position given by the third parameter. Otherwise, it stores 0.
     8 : (m,p) => {
       const modes = extract(memory[pos]);
-      console.log("equals memory=",m[p+0],m[p+1],m[p+1]);
-      console.log("equals modes =",modes);
+      // console.log("equals memory=",m[p+0],m[p+1],m[p+1]);
+      // console.log("equals modes =",modes);
       const v1 = val(m,pos+1,modes[0]);
       const v2 = val(m,pos+2,modes[1]);
       result = (v1 == v2) ? 1 : 0;
-      console.log(`setting pos ${m[p+3]} to ${result}`)
+      // console.log(`setting pos ${m[p+3]} to ${result}`)
       m[m[p+3]] = result;
       return p+4; // next command
     },
   }
 
-  var pos = 0;
+  var memory, pos;
+  [memory,pos] = state;
+
   var cnt = 0;
   while( memory[pos] != 99 ){
     cnt += 1;
     if( cnt > 1000 ) break; //failsafe
     const opcode = memory[pos] % 100;
-    console.log(`\n[${cnt}] pos=${pos}: raw=${memory[pos]} opcode=${opcode}`);
+    // console.log(`\n[${cnt}] pos=${pos}: raw=${memory[pos]} opcode=${opcode}`);
     pos = commands[opcode](memory,pos);
+    if( opcode == 4 ) {
+      // just executed the OUTPUT command; halt the pgm
+      // and return the state (so it can restart)
+      return [memory, pos]; 
+    }
   }
   console.log("program ended!")
-}
-
-function execute(program, values){
-  // this wrapper allows multiple executions
-  const memory = program.slice(0);
-  input = values;
-  index = 0;
-  run(memory)
-  return output;
-}
-
-function test(pgm, input, exout){
-  console.log(`\n\n******* test: ${pgm}`)
-  const out = execute(pgm,input); 
-  console.log(`test: output = ${out}`)
-  assert( out == exout );
-}
-
-function run_tests(){
-  // Using position mode, consider whether the input is equal to 8; output 1 (if it is) or 0 (if it is not).
-  const test1 = [3,9,8,9,10,9,4,9,99,-1,8];
-  test(test1,8,1); 
-  test(test1,9,0);
-  // Using position mode, consider whether the input is less than 8; output 1 (if it is) or 0 (if it is not).
-  const test2 = [3,9,7,9,10,9,4,9,99,-1,8];
-  test(test2,7,1); 
-  test(test2,9,0);
-  // Using immediate mode, consider whether the input is equal to 8; output 1 (if it is) or 0 (if it is not).
-  const test3 = [3,3,1108,-1,8,3,4,3,99];
-  test(test3,8,1); 
-  test(test3,9,0);
-  // Using immediate mode, consider whether the input is less than 8; output 1 (if it is) or 0 (if it is not).
-  const test4 = [3,3,1107,-1,8,3,4,3,99];
-  test(test4,7,1); 
-  test(test4,9,0);
-  // Here are some jump tests that take an input, then output 0 if the input was zero or 1 if the input was non-zero:
-  const test5 = [3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9] // using position mode
-  test(test5,0,0); // input is zero
-  test(test5,9,1); // input is NOT zero
-  const test6 = [3,3,1105,-1,9,1101,0,0,12,4,12,99,1] // using immediate mode
-  test(test6,0,0); // input is zero
-  test(test6,9,1); // input is NOT zero
-
-  // The next example program uses an input instruction to ask for a single number. The program will then output 999
-  // if the input value is below 8, output 1000 if the input value is equal to 8, or output 1001 if the input value is 
-  // greater than 8.
-  test0 = [3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99];
-  test(test0,7, 999); // lt 8
-  test(test0,8,1000); // eq 8
-  test(test0,9,1001); // gt8
+  return null;
 }
 
 function perm(xs) {
@@ -238,41 +197,52 @@ function perm(xs) {
   return ret;
 }
 
-function run_arrays(program, phases){
-  console.log(phases)
-  console.log(program)
-
-  // At its first input instruction, provide it the amplifier's phase setting 
-  // At its second input instruction, provide it the input signal; 0 as the first
-
-  var signal = 0;
-  for( var i=0; i<5; i++ ){
-    const phase = phases[i];
-    console.log(`\n\n******** amp${0} phase=${phase} signal=${signal} `);
-    signal = execute(program,[phase,signal]); 
-    console.log(`output signal = ${signal}`);
-  }
-  return signal;
-}
-
 function run_feedback(program, phases){
 
-  // 1st... is the phase
-  // all subsequent are signals
+  // 1st signal is Zero
+  // for each amp...
+  //    1st input... is its phase
+  //    all subsequent inputs are signals from prev
   // do not "restart" the program for each amp
+  // so keep 5 copies of the program state so that
+  // we can restart the execution at the same place
+  const states = _.range(0,6).map(x=>[program.slice(0),0])
 
-  var signal = 0;
+  // first pass through the loop is special because
+  // we pass two inputs... phase, then signal
+  
+  signal = 0;
   for( var i=0; i<5; i++ ){
     const phase = phases[i];
-    console.log(`\n\n******** amp${0} phase=${phase} signal=${signal} `);
-    signal = execute(program,[phase,signal]); 
-    console.log(`output signal = ${signal}`);
+    const state = states[i];
+    console.log(`\n***INITIAL amp${i} phase=${phase} signal=${signal} `);
+    const result = run(state,[phase,signal]);
+    states[i] = result; // save for next exec
+    console.log(`output signal = ${output}`);
+    signal = output;
   }
-  return signal;
+
+  // subsequent passes through the loop has just signal
+  // as the input
+
+  var pass = 0;
+  while( signal != null ){
+    for( var i=0; i<5; i++ ){
+      const phase = phases[i];
+      const state = states[i];
+      console.log(`\n****SUBSEQ ${pass++} amp${i} phase=${phase} signal=${signal} `);
+      const result = run(state,[signal]);
+      console.log(`output signal = ${output}`);
+      if( result == null ) return output
+      states[i] = result; // save for next exec
+      signal = output;
+    }
+  }
+  return 0; // error
 }
 
 //------------------------------------------------------------------------------
-// the real challenges are in the file
+// 
 //------------------------------------------------------------------------------
 
 filename = process.argv[2];
@@ -280,24 +250,21 @@ fs.readFile(filename, 'utf8', function(err, contents) {
     const lines = contents.split("\n");
     const phases = lines[0].split(",").map(x => parseInt(x));
     const program = lines[1].split(",").map(x => parseInt(x));
-    const result = parseInt(lines[2]);
+    const expected_result = parseInt(lines[2]);
 
-    const sig = run_arrays(program, phases)
+    // test case
+    // const sigout = run_feedback(program, phases)
+    // console.log(`final signal = ${sigout}`);
+    // assert( sigout == expected_result );
 
-    assert(sig == result);
-
-    const signals = [];
-    const permutations = perm([0,1,2,3,4]);
+    // search for optimal phase settings
+    const permutations = perm([5,6,7,8,9]);
+    var biggie = 0;
     _.each(permutations,phases=>{
-      const sig = run_arrays(program, phases)
-      console.log(sig, phases);
-      signals.push(sig);
+      const signal = run_feedback(program, phases);
+      if( signal > biggie ){
+        biggie = signal
+      }
     });
-    console.log(signals);
-    console.log(_.max(signals));
-
-
-    // run in feedback mode
-
-
+    console.log("biggie = ",biggie)
 });
